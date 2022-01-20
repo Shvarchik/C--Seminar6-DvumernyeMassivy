5a360a81b5c63a8d8ca190bceb7b1dd6ec682a30
Inna <shvartz_i@mail.ru>
2022-01-20 12:11:13 +0300

    Добавлены задачи 6 и 9, надо довести до красоты

---
+++ Task9_SrArifmStolb/Program.cs
@@ -0,0 +1,50 @@
+﻿// Среднее арифметическое каждого из столбцов
+
+int[,] CreateArray (int m,int n)
+{
+    int[,] matrix = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i,j] = new Random().Next(1,11);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix (int[,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write ($"{matrix[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintArray (double [] array)
+{
+    for (int i=0;i<array.Length;i++)
+        Console.Write ($"{array[i],7:N3}");
+}
+var rand = new Random();
+int row = rand.Next(1,6);
+int column = rand.Next(1,6);
+Console.WriteLine ($"{row}, {column}");
+int [,] Matrix = CreateArray(row,column);
+Console.WriteLine("Исходный массив:");
+PrintMatrix (Matrix);
+Console.WriteLine();
+double sum = 0;
+double[] Summ = new double [column];
+for (int j=0;j<Matrix.GetLength(1);j++)
+    {
+        for (int i=0;i<Matrix.GetLength(0); i++)
+        {
+            sum = sum + Matrix [i,j];
+        }
+        Summ[j] = sum/row;
+        sum = 0;
+    }
+PrintArray(Summ);
