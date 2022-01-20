663106f916e1607cb0af2e869a7b00383fa34ba0
Inna <shvartz_i@mail.ru>
2022-01-20 03:55:21 +0300

    Добавлены задачи 1 и 3

---
+++ Task1_ShowMassiv/Program.cs
@@ -0,0 +1,38 @@
+﻿// Двумерный, целыми числами
+
+int[,] CreateArray(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    FillArray(matrix);
+    return matrix;
+}
+
+void FillArray (int[,] matrix)
+{
+    var rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(100);
+        }
+    }
+}
+void PrintMatrix (int[,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write ($"{matrix[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Задайте число строк массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Задайте число столцов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int [,] Matrix = CreateArray(m,n);
+PrintMatrix (Matrix);
---
+++ Task3_RandDoubleArray/Program.cs
@@ -0,0 +1,40 @@
+﻿// 49.Показать двумерный массив размером m×n заполненный вещественными числами
+
+double[,] CreateArray(int row, int column)
+{
+    double[,] matrix = new double[row, column];
+    FillArray(matrix);
+    return matrix;
+}
+
+void FillArray (double[,] matrix)
+{
+    var rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.NextDouble() * 100;
+        }
+    }
+}
+void PrintMatrix (double[,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write ($"{matrix[i,j],8:N3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Задайте число строк массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Задайте число столцов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Double [,] Matrix = CreateArray(m,n);
+PrintMatrix (Matrix);
+
+
